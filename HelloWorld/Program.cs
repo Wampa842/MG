aa005212f0dad7429d546aa3bc0c1e31bb9bd04d
gabor.motko <gabor.motko.95@gmail.com>
2018-02-21 16:27:21 +0100

    initial commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Person
+    {
+        public enum GenderType { Male, Female, AttackHelicopter };
+        private string Name;
+        private int Age;
+        private GenderType Gender;
+    }
+
+    class Program
+    {
+        void Main(string args[])
+        {
+            
+        }
+    }
+}
